83d78576678738e6b85e8c49354ad4de19158649
kumabird <kumakumakuumakuuma@gmail.com>
2026-03-27 18:37:37 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Simulator from "@/pages/Simulator";
+import Leaderboard from "@/pages/Leaderboard";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Simulator} />
+      <Route path="/leaderboard" component={Leaderboard} />
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
